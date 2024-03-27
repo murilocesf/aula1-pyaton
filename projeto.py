8c37f764f315de13b240f0a199d1cd16b77f51a6
murilocesf <162130133+murilocesf@users.noreply.github.com>
2024-03-27 10:59:35 -0300

    avaliacao1

---
+++ projeto.py
@@ -0,0 +1,12 @@
+import os
+
+
+#print ("Olá mundo cruel")
+print("O 3DS e a turma numero 1 do sagrada")
+turma="3DS"
+pos=1
+print("o" ,turma,"e a turma numero",pos,"do sagrada")
+print(type(turma))
+print(type(pos))
+os.sytem("cls")
+
